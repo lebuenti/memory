5d725529181b15440fcc249dea26ed2012d0369a
leabuenti <leabuenti@gmail.com>
2020-02-13 16:37:10 +0100

    Add Webpack-html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,6 +7,19 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    })
+  ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
