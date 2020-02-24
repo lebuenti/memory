c278f4a68a368d07be0c89ce0dd3870cfa5392eb
lebuenti <leabuenti@gmail.com>
2020-02-24 19:52:59 +0100

    Insert style and sass-loader
    with examples

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import App from './components/App'
 import ReactDOM from "react-dom";
+import './style/main.scss';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 console.log("hello my name is lea");
