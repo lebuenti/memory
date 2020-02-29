aa938dda3b0d4ef3098eae5f1ba0ba426f707bbb
lebuenti <leabuenti@gmail.com>
2020-02-29 20:53:10 +0100

    Create Standard template on index
    footer and header

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,24 @@ import React, {Component} from "react";
 
 class App extends Component {
     render() {
-        return <div>
-            <h1>Hello world</h1>
-        </div>
+        return <div><h1>llllloremloremlorem
+            llllloremloremloremllllloremloremloremllllloremloremloremllllloremloremlorem
+            llllloremloremloremllllloremloremlorem
+            llllloremloremloremllllloremloremloremllllloremloremloremllllloremloremlorem
+
+            llllloremloremloremllllloremloremlorem
+            llllloremloremlorem
+            llllloremloremlorem
+            llllloremloremlorem
+            llllloremloremlorem
+            llllloremloremlorem
+            llllloremloremlorem
+            llllloremloremlorem
+            llllloremloremlorem
+            llllloremloremloremllllloremloremlorem
+
+
+        </h1></div>;
     }
 }
 
