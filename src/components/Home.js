cf2276f34be412b4dd34564c75629fe81c859838
lebuenti <leabuenti@gmail.com>
2020-03-01 12:20:07 +0100

    Create empty Home and Profile Component

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class Home extends Component {
+    render() {
+        return <div>Hallo mir geht es gut, danke der Nachfrage.</div>
+
+    }
+}
+
+export default Home;
---
+++ src/components/Profile.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+class Profile extends Component {
+    render() {
+        return <div>Ich bin eine ProfilSeite</div>;
+    }
+}
+
+export default Profile;
