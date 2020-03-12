83dfa30815e105222d75143fcdb84e7ee2bc0eea
lebuenti <leabuenti@gmail.com>
2020-03-12 20:48:43 +0100

    Add Button

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,15 @@ import React, {Component} from "react";
 
 class Home extends Component {
     render() {
-        return <div>Hallo mir geht es gut, danke der Nachfrage.</div>
-
+        return <>
+            <div className="row">
+                <div className="col">
+                    <button className='add'>
+                        <i className="fas fa-plus"/>
+                    </button>
+                </div>
+            </div>
+        </>
     }
 }
 
