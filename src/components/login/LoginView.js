e04e8e8bd2a47ed860d35caad2b8a72816fb95c4
lebuenti <leabuenti@gmail.com>
2020-04-01 17:10:51 +0200

    Show Login after press back Button in browser

--- src/components/login/LoginView.js
+++ src/components/login/LoginView.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import "../../style/login.scss"
 import "../../style/main.scss"
 import Login from "./Login";
@@ -7,10 +7,27 @@ import Register from "./Register";
 export default function LoginView() {
     const [showRegister, setShowRegister] = useState(false);
 
+    useEffect(() => {
+        window.onpopstate = () => decideContent();
+        return () => window.onpopstate = undefined;
+    }, []);
+
+    useEffect(() => {
+        decideContent();
+    }, []);
+
+    const decideContent = () => {
+        if (location.pathname.endsWith('register')) {
+            setShowRegister(true);
+        } else if (location.pathname.endsWith('/')) {
+            setShowRegister(false)
+        }
+    };
+
     return <div id="loginBody">
         <div id="login">
             <div className="row">
-                {showRegister || location.pathname.endsWith('register') ? <Register/> : <Login showRegister={(value) => {
+                {showRegister ? <Register/> : <Login showRegister={(value) => {
                     setShowRegister(value);
                     history.pushState({}, '', 'register');
                 }}/>}
