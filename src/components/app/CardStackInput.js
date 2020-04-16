9ff8cd6efb28db3a8aa100f16730325f4b3853cb
lebuenti <leabuenti@gmail.com>
2020-04-16 12:33:49 +0200

    Create CardStackInput.js

---
+++ src/components/app/CardStackInput.js
@@ -0,0 +1,67 @@
+import React, {useState} from "react"
+import toast from "../../toast/toast";
+
+export default function CardStackInput(props) {
+    const [inputError, setInputError] = useState({cardStackName: false});
+    const [cardStack, setCardStack] = useState({name: ''});
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        setInputError({
+            cardStackName: cardStack.name.length === 0
+        });
+        if (cardStack.name.length === 0) {
+            toast.fail('Name of card stack is empty');
+            return;
+        }
+        handleReset();
+
+        props.submit(cardStack).catch((error) => {
+            toast.fail('Could not save card stack into database');
+            console.error(error);
+        });
+    };
+
+    const handleReset = () => {
+        props.setShowInput(false);
+        setCardStack({name: ''});
+        setInputError({cardStackName: false});
+    };
+
+    return <div className="cards">
+        <div className="row">
+            <div className="card cardBack"/>
+        </div>
+        <div className="row">
+            <div className="card cardMiddle"/>
+        </div>
+        <div className="row">
+            <div className="card cardFront">
+                <div className="row formAdd">
+                    <form onSubmit={handleSubmit}>
+                        <div className="col">
+                            <h3>New card stack</h3>
+                        </div>
+                        <div className="col">
+                            <label>
+                                Name
+                                <input className={(inputError.cardStackName ? 'inputError' : '')}
+                                       type="text" value={cardStack.name} placeholder="Geometry"
+                                       onChange={e => setCardStack({name: e.target.value})}/>
+                            </label>
+                        </div>
+                        <div className="col">
+                            <button type="reset" className="buttonReset button" onClick={handleReset}>
+                                <i className="fas fa-times icon"/>
+                            </button>
+                            <button type="submit" className="buttonSuccess button">
+                                <i className="fas fa-check icon"/>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+}
--- src/components/app/Subject.js
+++ src/components/app/Subject.js
@@ -2,12 +2,11 @@ import React, {useEffect, useState} from "react";
 import CardStack from "./CardStack";
 import toast from "../../toast/toast";
 import db from "../../db/db";
+import CardStackInput from "./CardStackInput";
 
 export default function Subject(props) {
     const [cardStacks, setCardStacks] = useState([]);
-    const [cardStack, setCardStack] = useState({name: ''});
     const [showInput, setShowInput] = useState(false);
-    const [inputError, setInputError] = useState({cardStackName: false});
 
     useEffect(() => {
         db.getAllCardStacksFromSubject(props.id)
@@ -22,33 +21,11 @@ export default function Subject(props) {
         );
     }, []);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        setInputError({
-            cardStackName: cardStack.name.length === 0
-        });
-
-        if (cardStack.name.length === 0) {
-            toast.fail('Name of card stack is empty');
-            return;
-        }
-
-        db.addCardStack(props.id, cardStack.name)
+    const submit = (cardStack) => {
+        return db.addCardStack(props.id, cardStack.name)
             .then((dbCardStack) => {
                 setCardStacks(curr => [dbCardStack, ...curr]);
-                handleReset();
-            }).catch((error) => {
-                toast.fail('Could not save card stack into database');
-                console.error(error);
-            }
-        );
-    };
-
-    const handleReset = () => {
-        setShowInput(false);
-        setCardStack({name: ''});
-        setInputError({cardStackName: false});
+            });
     };
 
     return <div className="subject" style={{backgroundColor: props.color}}>
@@ -69,41 +46,7 @@ export default function Subject(props) {
         </div>
 
         <div className="row newCards" style={{display: showInput ? 'flex' : 'none'}}>
-            <div className="cards">
-                <div className="row">
-                    <div className="card cardBack"/>
-                </div>
-                <div className="row">
-                    <div className="card cardMiddle"/>
-                </div>
-                <div className="row">
-                    <div className="card cardFront">
-                        <div className="row formAdd">
-                            <form onSubmit={handleSubmit}>
-                                <div className="col">
-                                    <h3>New card stack</h3>
-                                </div>
-                                <div className="col">
-                                    <label>
-                                        Name
-                                        <input className={(inputError.cardStackName ? 'inputError' : '')}
-                                               type="text" value={cardStack.name} placeholder="Geometry"
-                                               onChange={e => setCardStack({name: e.target.value})}/>
-                                    </label>
-                                </div>
-                                <div className="col">
-                                    <button type="reset" className="buttonReset button" onClick={handleReset}>
-                                        <i className="fas fa-times icon"/>
-                                    </button>
-                                    <button type="submit" className="buttonSuccess button">
-                                        <i className="fas fa-check icon"/>
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <CardStackInput submit={(cardStack) => submit(cardStack)} setShowInput={(value) => setShowInput(value)}/>
         </div>
 
         <div id="oldCards">
